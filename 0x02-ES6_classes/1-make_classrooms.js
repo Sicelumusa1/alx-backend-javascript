f52b144c6fb661da5a8775c9cb2cb81fa1debf4b
Sicelumusa <musaqwabe.com>
2024-03-07 17:47:06 +0000

    fixed function declaration

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 import ClassRoom from './0-classroom';
-export default initializeRooms {
+export default initializeRooms() {
     const roomSizes = [19, 20, 34];
     const rooms = roomSizes.map(size => new ClassRoom(size));
     return rooms;
