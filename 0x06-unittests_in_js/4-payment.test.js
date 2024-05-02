8e305adf6bc4ecbe3a100d7fc3463bc9b289adf1
Sicelumusa <musaqwabe@gmail.com>
2024-05-02 19:05:22 +0000

    fixed typos

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -7,13 +7,13 @@ const sendPaymentRequestToApi = require('./4-payment.js');
 describe('sendPaymentRequestToApi', () => {
   it('should use Utils.calculateNumber with "SUM", 100, 20 and stub to return 10', () => {
     const spy = sinon.spy(console, 'log');
-    const stup = sinon.stub(Utils, 'calculateNumber').return(10);
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
     sendPaymentRequestToApi(100, 20);
     
     expect(stub.calledOnce).to.be.true;
     expect(stub.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spy.colledOnceWithExactly('The total is: 10')).to.be.true;
+    expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
 
     stub.restore();
     spy.restore();
