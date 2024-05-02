f3b674877a775451fc4c48c545dc144a53a03723
Sicelumusa <musaqwabe@gmail.com>
2024-05-02 18:46:26 +0000

    Fixed typos

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,10 +6,10 @@ const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi', () => {
   it('should use Utils.calculateNumber with "SUM", 100, 20', () => {
-    const spy = simon.spy(Utils, 'calculateNumber');
+    const spy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
-    
+
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
 
