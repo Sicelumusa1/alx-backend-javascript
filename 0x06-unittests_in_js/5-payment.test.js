7f262cd57fe5f2d4f61a2c6267eff2edb3479140
Sicelumusa <musaqwabe@gmail.com>
2024-05-03 06:49:50 +0000

    Fixed a typo in function 'sendPaymentRequestToAPI' and added the missing paranthesis

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,9 +1,9 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const expect = chai.expect;
-const sendPaymentRequstToApi = require('./5-payment.js');
+const sendPaymentRequestToApi = require('./5-payment.js');
 
-describe('sendPaymentRequstToApi', () => {
+describe('sendPaymentRequestToApi', () => {
   let consoleSpy;
 
   beforeEach(() => {
@@ -15,14 +15,14 @@ describe('sendPaymentRequstToApi', () => {
   })
 
   it('should log "The total is: 120" when called with 100 and 20', () => {
-    sendPaymentRequstToApi(100, 20);
-    expect(consoleSpy.calledOnceWithExactly('The total is: 120').to.be.true;
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
     expect(consoleSpy.callCount).to.equal(1);
   });
 
   it('should log "The total is: 20" when called with 10 and 10', () => {
-    sendPaymentRequstToApi(10, 10);
-    expect(consoleSpy.calledOnceWithExactly('The total is: 20').to.be.true;
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
     expect(consoleSpy.callCount).to.equal(1);
   });
 })
