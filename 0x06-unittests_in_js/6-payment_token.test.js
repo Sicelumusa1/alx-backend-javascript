87facacfd3b23eb676af5d9da3cbd1b600b91742
Sicelumusa <musaqwabe@gmail.com>
2024-05-03 06:56:32 +0000

    fixed the filename being required

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const getPaymentTokenFromAPI = require('./6-pyment_token.js');
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
 
 describe('getPaymentTokenFromAPI',() => {
 
