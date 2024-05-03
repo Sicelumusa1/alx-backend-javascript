cbd761092ac1347c458173fe62f8fc3c4be41762
Sicelumusa <musaqwabe@gmail.com>
2024-05-03 07:44:49 +0000

    fixed typos and chai.request(app) to make a request

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,16 +3,19 @@ const expect = chai.expect;
 const request = require('request');
 const app = require('./api.js');
 
-describe('Index Page' () => {
+describe('Index Page', () => {
   before((done) => {
     app.listen(7865, done);
   });
 
   it('should return the correct status code and message for GET /', (done) => {
-    request('http://localhost:7865/', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
+    chai
+      .request(app)
+      .get('/')
+      .end((error, response) => {
+        expect(response).to.have.equal(200);
+        expect(response.text).to.equal('Welcome to the payment system');
+        done();
     });
   });
 });
