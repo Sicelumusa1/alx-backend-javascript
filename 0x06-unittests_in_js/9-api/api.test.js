617a2638106fb26735a78595f117a7387d8c6701
Sicelumusa <musaqwabe@gmail.com>
2024-05-03 09:31:41 +0000

    used chai-http

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,36 +1,67 @@
 const chai = require('chai');
 const expect = chai.expect;
-const request = require('request');
+const chaiHttp = require('chai-http');
 const app = require('./api.js');
 
-describe('Index Page' () => {
+chai.use(chaiHttp);
+
+describe('Index Page', () => {
+  let server;
+
   before((done) => {
-    app.listen(7865, done);
+    server = app.listen(7865, done);
   });
 
   it('should return the correct status code and message for GET /', (done) => {
-    request('http://localhost:7865/', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
+    chai
+      .request(app)
+      .get('/')
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        expect(response.text).to.equal('Welcome to the payment system');
+        done();
+      });
   });
-});
+
+  after((done) => {
+    server.close(done);
+  });
+})
 
 describe('Cart Page', () => {
-  it('should return 200 for valid cart ID', (done) => {
-    request('http://localhost:7865/cart/123', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 123');
-      done();
-    });
+  let server;  
+
+  before((done) => {
+    server = app listen(7865, done);
   });
 
-  it('should return 404 for valid cart ID', (done) => {
-    request('http://localhost:7865/cart/abc', (error, response, body) => {
-      expect(response.statusCode).to.equal(404);
-      expect(body).to.equal('Invalid cart ID');
-      done();
+  it('should return 200 for valid cart ID', (done) => {
+    chai
+      .request(app)
+      .get('/cart/123')
+      .end((error, response) => {
+        if (error) {
+	  done(error);
+          return;
+	}
+        expect(response).to.have.status(200);
+        expect(response.text).to.equal('Payment methods for cart 123');
+        done();
+      });
     });
-  });
+
+    it('should return 404 for valid cart ID', (done) => {
+      chai
+        .request(app)
+	.get('/cart/abc')
+	.end((error, response) => {
+	   if (error) {
+            done(error);
+            return;
+           }
+           expect(response).to.have.status(404);
+           expect(response.text).to.equal('Invalid cart ID');
+           done();
+        });
+     });
 })
